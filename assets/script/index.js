bfbcdc87d1903a6ca7f2b520b975c9eee5f8dcf8
Samuel Yazie <samuelwassie169@gmail.com>
2023-11-24 14:26:01 -0600

    adding code in

---
+++ assets/script/index.js
@@ -0,0 +1,109 @@
+'use strict';
+
+import { onEvent, select, selectById, selectAll, print } from "./utility.js";
+
+const contacts = selectById('contactsContainer');
+const counter = selectById('counter');
+const contactInput = selectById('contactInput');
+const add = selectById('add');
+const corrections = select('.corrections');
+const contactsArray = [];
+
+class Contact {
+    #name;
+    #city;
+    #email;
+
+    constructor(name, city, email) {
+        this.#name = name;
+        this.#city = city;
+        this.#email = email;
+    }
+
+    get name() {
+        return this.#name;
+    }
+
+    get city() {
+        return this.#city;
+    }
+
+    get email() {
+        return this.#email;
+    }
+}
+
+function listContacts(name, city, email) {
+    const newContact = new Contact(name, city, email);
+    contactsArray.unshift(newContact);
+
+    const contactDiv = document.createElement('div');
+    contactDiv.className = 'contact';
+    
+    const nameParagraph = document.createElement('p');
+    const cityParagraph = document.createElement('p');
+    const emailParagraph = document.createElement('p');
+
+    nameParagraph.textContent = `Name: ${newContact.name}`;
+    cityParagraph.textContent = `City: ${newContact.city}`;
+    emailParagraph.textContent = `Email: ${newContact.email}`;
+
+    contactDiv.appendChild(nameParagraph);
+    contactDiv.appendChild(cityParagraph);
+    contactDiv.appendChild(emailParagraph);
+    onEvent('click', contactDiv, () => {
+        removeContact(contactDiv, newContact)
+    });
+
+    contacts.appendChild(contactDiv);
+    contactInput.value = '';
+
+    updateCounter();
+}
+
+function getValidation(name, city, email) {
+    const nameAndCityRegex = /^[A-Za-z\s]+$/;
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (nameAndCityRegex.test(name) && nameAndCityRegex.test(city) && emailRegex.test(email)) {
+        listContacts(name, city, email)
+    } else {
+        corrections.innerText = 'Invalid input!';
+    }
+}
+
+function addContact() {
+    const contactInfo = contactInput.value;
+    const contactValues = contactInfo.split(',').map(value => value.trim());
+
+    if (contactValues.length === 3) {
+        if (contactsArray.length < 10) {
+            getValidation(contactValues[0], contactValues[1], contactValues[2]);
+            corrections.innerText = '';
+        } else {
+            corrections.innerText = 'You have reached the maximum number of contacts!';
+        }
+    } else {
+        corrections.innerText = 'Please provide Name, City, and Email separated by commas.';
+    }
+}
+
+function removeContact(contactDiv, contact) {
+    const index = contactsArray.indexOf(contact);
+    contactsArray.splice(index, 1);
+    contacts.removeChild(contactDiv);
+    corrections.innerText = '';
+    updateCounter();
+}
+
+function updateCounter() {
+    counter.textContent = `Contacts: ${contactsArray.length}`;
+}
+
+onEvent('click', add, (event) =>{
+    event.preventDefault();
+    addContact();
+});
+
+onEvent('load', window, () => {
+    contactInput.value = '';
+});
